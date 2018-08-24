0eee4ae75a28de22509ce9feee5e6e7240864252
tianxy <791085869@qq.com>
2018-08-24 17:49:38 +0800

    添加数据库操作目录

---
+++ Tianxy.Web/Tianxy.Tool/DatabaseHelper/DataBaseHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tianxy.Tool.DatabaseHelper
+{
+    //==============================================================
+    //  作者：tianxy
+    //  时间：2018/8/24 17:13:47
+    //  文件名：DataBaseHelper
+    //  版本：V1.0.1  
+    //  说明： 
+    //  修改者：tianxy
+    //  修改说明： 
+    //==============================================================
+    class DataBaseHelper
+    {
+    }
+}
